eb664e44a092d78d83aef55e9e77f908390924f4
Nicholas Mendoza <nicholasmendoza@Nicholass-MBP.localdomain>
2025-01-19 14:44:31 -0800

    Implement CLI logic to accept port number as arg

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,24 @@
-const yargs = require('yargs/yargs')
+#! /usr/bin/env node
 
-if (yargs._ == null) {  
-    console.log("Requires 1 argument") 
-    return;  
-}
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers');
+const { exec } = require('child_process');
+
+const argv = yargs(hideBin(process.argv))
+    .demandCommand(1, 'You must provide a port number')
+    .check((argv) => {
+        if (argv._.length > 1) {
+            throw new Error('You must provide exactly 1 port number. Provided ' + argv._.length + ' arguments')
+        }
+        if (isNaN(argv._[0])) {
+            throw new Error('Port must be a number')
+        }
+        if (argv._[0] < 1 || argv._[0] > 65535) {
+            throw new Error('Port must be between 1 and 65535')
+        }
+        return true;
+    })
+    .help()
+    .argv;
+
+console.log('Port: ' + argv._[0])
